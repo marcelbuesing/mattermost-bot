9c04badca69a9151971bee377980b9d20b8e2891
marcelbuesing <buesing.marcel@googlemail.com>
2016-10-18 14:31:06 +0200

    Add SystemHook messages

--- src/Main.hs
+++ src/Main.hs
@@ -20,15 +20,24 @@ import           Web.Scotty
 import           MattermostBot.Data
 
 main :: IO ()
-main = scotty 3000 $
-  post "/triggerci" $ do
-    hdr <- header "X-Gitlab-Event"
-    evt <- jsonData :: ActionM GitlabEvent
+main = scotty 9666 $ do
+  post "/gitlab/webhooks" $ do
     cfg <- lift botConfig
     lift $ print $ show cfg
-    lift $ print $ show $ toJSON $ toSlack cfg evt
-    lift $ W.postWith opts (exportURL $ _botConfigMattermostIncoming cfg) (toJSON $ toSlack cfg evt)
-    lift $ print $ show $ encode $ toSlack cfg evt
+    hdr <- header "X-Gitlab-Event"
+    evt <- jsonData :: ActionM GitlabEvent
+    lift $ print $ show $ toJSON $ whToSlack cfg evt
+    lift $ W.postWith opts (exportURL $ _botConfigMattermostIncoming cfg) (toJSON $ whToSlack cfg evt)
+    lift $ print $ show $ encode $ whToSlack cfg evt
+    status ok200
+  post "/gitlab/systemhooks" $ do
+    cfg <- lift botConfig
+    lift $ print $ show cfg
+    hdr <- header "X-Gitlab-Event"
+    evt <- jsonData :: ActionM SystemHook
+    lift $ print $ show $ toJSON $ shToSlack cfg evt
+    lift $ W.postWith opts (exportURL $ _botConfigMattermostIncoming cfg) (toJSON $ shToSlack cfg evt)
+    lift $ print $ show $ encode $ shToSlack cfg evt
     status ok200
 
 -- | disable tls
@@ -43,17 +52,30 @@ defaultCfg = BotConfig "TownsSquare" "λmatterbot" ":ghost:" $ fromJust $ import
 readCfg :: IO (Maybe BotConfig)
 readCfg = decodeFile "./botconfig.yaml"
 
-toSlack :: BotConfig -> GitlabEvent -> SlackIncoming
-toSlack c (WHPushEvent _ _ _ _ _ _ userName _ _ _ _ project commits _) =
+whToSlack :: BotConfig -> GitlabEvent -> SlackIncoming
+whToSlack c (WHPushEvent _ _ _ _ _ _ userName _ _ _ _ project commits _) =
   toIncoming c (
   userName <> " pushed \n"
   <> T.unlines (pushCommitToMarkDown <$> commits)
   <> "\n to " <> _projectName project)
-toSlack c (IssueEvent _ user _ _ _ _) = toIncoming c (_userUserName user <> " modified issue")
-toSlack c (PipelineEvent _ _ user project commit builds) =
+whToSlack c (IssueEvent _ user _ _ _ _) = toIncoming c (_userUserName user <> " modified issue")
+whToSlack c (PipelineEvent _ _ user project commit builds) =
   toIncoming c ("builds initiated by " <>  _userUserName user
   <> " in project " <> _pipelineEventProjectName project
   <> "\n" <> T.unlines (buildToMarkDown <$> builds))
+
+shToSlack :: BotConfig -> SystemHook-> SlackIncoming
+shToSlack c (SHPushEvent _ _ _ _ _ userName _ _ _ _ project commits _) =
+  toIncoming c (
+  userName <> " pushed \n"
+  <> T.unlines (pushCommitToMarkDown <$> commits)
+  <> "\n to " <> _projectName project)
+shToSlack c (ProjectCreated _ _ _ name _ ownerName _ pathWithNamespace id _) =
+  toIncoming c (":new: " <> ownerName <> " created a new project " <> name <> " " <> parenthesize  pathWithNamespace)
+shToSlack c (ProjectDestroyed _ _ _ name _ ownerName _ pathWithNamespace _ _) =
+  toIncoming c (":x: project " <> name <> " deleted " <> parenthesize pathWithNamespace)
+shToSlack c (ProjectRenamed _ _ _ name _ ownerName _ pathWithNamespace pathWithNamespaceOld _ _) =
+  toIncoming c (":pencil: project " <> name <> " renamed " <> parenthesize (pathWithNamespaceOld <> " -> " <> pathWithNamespace))
 
 pushCommitToMarkDown :: Commit -> T.Text
 pushCommitToMarkDown c = "- " <> "[" <> _commitMessage c <> "]" <> "(" <> T.pack (exportURL $ _commitUrl c) <> ")"
@@ -63,3 +85,6 @@ buildToMarkDown b = "id: " <> T.pack (show $ _buildId b) <> " status: " <> _buil
 
 toIncoming :: BotConfig -> T.Text -> SlackIncoming
 toIncoming c t = SlackIncoming t (_botConfigChannel c) (_botConfigUsername c) (_botConfigIconEmoij c)
+
+parenthesize :: T.Text -> T.Text
+parenthesize inner = "(" <> inner <> ")"

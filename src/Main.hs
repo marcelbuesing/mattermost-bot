5ff228ca5db72b19370c417bae3cf860de4e773f
marcelbuesing <buesing.marcel@googlemail.com>
2016-11-18 09:11:52 +0100

    Retrieve commit details via Gitlab API

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,13 @@
 module Main where
 
 import           Control.Lens
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Data.Aeson
 import           Data.Maybe                  (fromJust, fromMaybe)
 import           Data.Monoid                 ((<>))
 import qualified Data.Text as T
+import           Data.Text.Encoding          (encodeUtf8)
 import           Data.Yaml                   (decodeFile)
 import           GitlabApi.Data
 import           Network.Connection          (TLSSettings (..))
@@ -28,18 +30,15 @@ main = scotty 9666 $ do
     lift $ print $ show cfg
     hdr <- header "X-Gitlab-Event"
     evt <- jsonData :: ActionM GitlabEvent
-    lift $ print $ show $ toJSON $ whToSlack cfg evt
     lift $ W.postWith opts (exportURL $ _botConfigMattermostIncoming cfg) (toJSON $ whToSlack cfg evt)
-    lift $ print $ show $ encode $ whToSlack cfg evt
     status ok200
   post "/gitlab/systemhooks" $ do
     cfg <- lift botConfig
     lift $ print $ show cfg
     hdr <- header "X-Gitlab-Event"
     evt <- jsonData :: ActionM SystemHook
-    lift $ print $ show $ toJSON $ shToSlack cfg evt
-    lift $ W.postWith opts (exportURL $ _botConfigMattermostIncoming cfg) (toJSON $ shToSlack cfg evt)
-    lift $ print $ show $ encode $ shToSlack cfg evt
+    out <- lift $ toJSON <$> shToSlack cfg evt
+    lift $ W.postWith opts (exportURL $ _botConfigMattermostIncoming cfg) out
     status ok200
 
 -- | disable tls
@@ -49,7 +48,10 @@ botConfig :: IO BotConfig
 botConfig = fromMaybe defaultCfg <$> readCfg
 
 defaultCfg :: BotConfig
-defaultCfg = BotConfig "TownsSquare" "λmatterbot" ":ghost:" $ fromJust $ importURL "mattermostIncoming"
+defaultCfg =
+  let matterMostUrl = fromJust $ importURL "mattermostIncoming"
+      gitlabUrl = fromJust $ importURL "https://gitlab.example.com"
+  in BotConfig "TownsSquare" "λmatterbot" ":ghost:" matterMostUrl gitlabUrl "tkn"
 
 readCfg :: IO (Maybe BotConfig)
 readCfg = decodeFile "./botconfig.yaml"
@@ -66,19 +68,25 @@ whToSlack c (PipelineEvent _ _ user project commit builds) =
   <> " in project " <> _pipelineEventProjectName project
   <> "\n" <> T.unlines (buildToMarkDown <$> builds))
 
-shToSlack :: BotConfig -> SystemHook-> SlackIncoming
-shToSlack c (SHPushEvent _ _ _ sha _ userName _ _ _ _ project commits _) =
-  toIncoming c (
-  userName <> " pushed "
-  <> "[" <> sha <> "](" <> commitHomepage project sha <> ")"
-  <> T.unlines (pushCommitToMarkDown <$> commits)
-  <> " to " <> "[" <> _projectName project <> "](" <> (T.pack $ exportURL $ _projectHomepage project) <> ")")
+shToSlack :: BotConfig -> SystemHook-> IO SlackIncoming
+shToSlack c (SHPushEvent _ _ _ sha _ userName _ _ pid _ project commits _) = do
+  cd <- commitDetails c pid sha
+  let additions = T.pack $ show $ _commitStatsAdditions $ _commitSingleStats $ cd
+      deletions = T.pack $ show $ _commitStatsDeletions $ _commitSingleStats $ cd
+      projectUrl = "[" <> _projectName project <> "](" <> (T.pack $ exportURL $ _projectHomepage project) <> ")"
+      shaUrl = "[" <> sha <> "](" <> commitHomepage project sha <> ")"
+  return $ toIncoming c (
+    userName <> " pushed " <> " to " <> projectUrl
+    <> "\nRef:\t\t\t " <> shaUrl
+    <> "\nMessage:\t " <> _commitSingleMessage cd
+    <> "Stats:\t\t " <> "(+" <> additions <> " lines / -" <> deletions <> " lines)"
+    <> "\nTime:\t\t " <> _commitSingleCommittedDate cd)
 shToSlack c (ProjectCreated _ _ _ name _ ownerName _ pathWithNamespace id _) =
-  toIncoming c (":new: " <> ownerName <> " created a new project " <> name <> " " <> parenthesize  pathWithNamespace)
+  return $ toIncoming c (":new: " <> ownerName <> " created a new project " <> name <> " " <> parenthesize  pathWithNamespace)
 shToSlack c (ProjectDestroyed _ _ _ name _ ownerName _ pathWithNamespace _ _) =
-  toIncoming c (":x: project " <> name <> " deleted " <> parenthesize pathWithNamespace)
+  return $ toIncoming c (":x: project " <> name <> " deleted " <> parenthesize pathWithNamespace)
 shToSlack c (ProjectRenamed _ _ _ name _ ownerName _ pathWithNamespace pathWithNamespaceOld _ _) =
-  toIncoming c (":pencil: project " <> name <> " renamed " <> parenthesize (pathWithNamespaceOld <> " -> " <> pathWithNamespace))
+  return $ toIncoming c (":pencil: project " <> name <> " renamed " <> parenthesize (pathWithNamespaceOld <> " -> " <> pathWithNamespace))
 
 pushCommitToMarkDown :: Commit -> T.Text
 pushCommitToMarkDown c = "- " <> "[" <> _commitMessage c <> "]" <> "(" <> T.pack (exportURL $ _commitUrl c) <> ")"
@@ -93,4 +101,13 @@ parenthesize :: T.Text -> T.Text
 parenthesize inner = "(" <> inner <> ")"
 
 commitHomepage :: Project -> T.Text -> T.Text
-commitHomepage p sha = (T.pack $ exportURL $ _projectHomepage p) <> "/commit/" <> sha
+commitHomepage p sha = T.pack (exportURL $ _projectHomepage p) <> "/commit/" <> sha
+
+commitDetails :: BotConfig -> ProjectId -> CommitRef -> IO CommitSingle
+commitDetails c pid cr = do
+  resp <- let gitlabApi = _botConfigGitlabApiUrl c
+              url =  (exportURL gitlabApi) <> "/api/v3/projects/" <> (show pid) <> "/repository/commits/" <> (T.unpack cr)
+              tkn = encodeUtf8 (_botConfigGitlabApiPrivateToken c)
+              opt = W.defaults & W.header "PRIVATE-TOKEN" .~ [tkn]
+          in W.asJSON =<< W.getWith opt url :: IO (W.Response CommitSingle)
+  return $ resp ^. W.responseBody
--- src/MattermostBot/Data/Config.hs
+++ src/MattermostBot/Data/Config.hs
@@ -2,17 +2,24 @@
 module MattermostBot.Data.Config where
 
 import Data.Maybe (fromJust)
+import Data.Text as T
 import qualified Data.Yaml as Y
 import Data.Yaml (FromJSON(..), (.:))
 import Network.URL (URL, importURL)
 
 import MattermostBot.Data.Slack
 
+
+type GitlabApiPrivateToken = Text
+
 data BotConfig = BotConfig
   { _botConfigChannel :: SlackChannel
   , _botConfigUsername :: SlackUsername
   , _botConfigIconEmoij :: SlackIconEmoji
   , _botConfigMattermostIncoming :: URL
+  -- | base url e.g. https://gitlab.example.com
+  , _botConfigGitlabApiUrl :: URL
+  , _botConfigGitlabApiPrivateToken :: GitlabApiPrivateToken
   } deriving Show
 
 instance FromJSON BotConfig where
@@ -21,5 +28,7 @@ instance FromJSON BotConfig where
     v .:   "channel" <*>
     v .:   "username" <*>
     v .:   "iconEmoij" <*>
-    ((fromJust . importURL) <$> v .:   "mattermostIncoming")
+    ((fromJust . importURL) <$> v .:   "mattermostIncoming") <*>
+    ((fromJust . importURL) <$> v .:   "gitlabApiUrl") <*>
+    v .:   "gitlabApiPrivateToken"
   parseJSON _ = fail "Expected Object for Config value"
